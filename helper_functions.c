987bb7127b54cca1124cac328f4784ae6e585fd3
Vincent Wanjohi <vwan045@gmail.com>
2022-11-16 10:35:16 +0300

    Updates

--- helper_functions.c
+++ helper_functions.c
@@ -13,3 +13,32 @@ void strip_line(char *str, ssize_t n)
     if (str[n - 1] == '\n')
         str[n - 1] = '\0';
 }
+/**
+ * _getenv - searched for specified environment variable
+ * @var_name: specified envt variable name
+ * Return: returns pointer to variable values or NULL
+ */
+
+char *_getenv(char *var_name)
+{   
+    char *var_vals = NULL;
+    int n, count, len;
+
+    n = strlen(var_name);
+    for (count = 0; environ[count] != NULL; count++)
+    {
+        if (strncmp(environ[count], var_name, n) == 0)
+        {
+            len = strlen(environ[count]);
+            var_vals = malloc(sizeof(char) * len + 1);
+            if (var_vals == NULL)
+            {
+                perror("malloc");
+                exit(EXIT_FAILURE);
+            }
+            strncpy(var_vals, (environ[count] + n + 1), len);
+            return (var_vals);
+        }
+    }
+    return (NULL);
+}
