2da2c90ee1f02e11772a97774eb0b22e1cef6d1d
Vincent <vwan045@gmail.com>
2024-01-18 10:36:10 +0300

    Corrections for Mandatory shell

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # runs a clean build
 
-declare -r APP="shell"
+declare -r APP="hsh"
 declare -ra CFLAGS=("-Wall" "-Wextra" "-Werror" "-pedantic" "-std=gnu89")
 declare -ra SRC=("input.c" "main.c" "parser.c" "utils.c" "strings.c" "strings2.c" "strings3.c" "mem.c" "executor.c" "builtin.c" "builds.c" "env.c" "scanner.c" "cmd.c")
 
